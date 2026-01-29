034f2b938791846aed7a0b93b81bf8e965aa56ec
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-01-29 20:16:28 +0530

    API Calling

--- 25 RTK Project/src/App.jsx
+++ 25 RTK Project/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import "./App.css";
+import { fetchPhotos, fetchvideos } from "./api/mediaApi";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <div className="h-screen w-full bg-gray-950 text-white">
+        <button
+          onClick={async () => {
+            const data = await fetchPhotos("dog");
+            console.log(data.results);
+          }}
+          className="bg-gray-500 p-2 rounded-full m-5"
+        >
+          Get photos
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+        <button
+          onClick={async () => {
+            const data = await fetchvideos("dog");
+            console.log(data.videos);
+          }}
+          className="bg-green-500 p-2 rounded-full m-5"
+        >
+          Get videos
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ 25 RTK Project/src/api/mediaApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
+
+export const fetchPhotos = async (query, page = 1, per_page = 20) => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    params: { query, page, per_page },
+    headers: { Authorization: `{Client-ID ${UNSPLASH_KEY}` },
+  });
+
+  return response.data;
+};
+
+export const fetchvideos = async (query, per_page = 20) => {
+  const response = await axios.get("https://api.pexels.com/videos/search", {
+    params: { query, per_page },
+    headers: { Authorization: PEXELS_KEY },
+  });
+
+  return response.data;
+};
+
